8bd54cf356b1ada01176ede23e830a9121cc2882
Yang <651033755@qq.com>
2020-12-03 23:22:15 +0800

    完成home页面的baner

---
+++ pages/home/home.js
@@ -0,0 +1,40 @@
+// pages/home/home.js
+Page({
+
+  data: {
+    indicatorDots: true,
+    indicatorDots:true,
+    autoplay: false,
+    interval: 2000,
+    duration: 500,
+    swiperArr:[
+      "/images/banner/baner1.jpg",
+      "/images/banner/baner2.jpg",
+      "/images/banner/baner3.jpg",
+      "/images/banner/baner4.jpg",
+    ],
+    cardArr:[
+      {
+     src:"/images/home/card01.jpg",
+     text:"给特别的你"
+    },
+    {
+      src:"/images/home/card02.jpg",
+      text:"我想对你说"
+     },
+     {
+      src:"/images/home/card03.jpg",
+      text:"勇敢爱"
+     },
+     {
+      src:"/images/home/card04.jpg",
+      text:"给特别的你"
+     },
+     {
+      src:"/images/home/card05.jpg",
+      text:"给特别的你"
+     },
+  ]
+  },
+
+})
